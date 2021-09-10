eb255a4a76f78665d94091562a10c41a88c3f6fc
evandikt <evandikt@gmail.com>
2021-09-10 10:16:30 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
-require('dotenv').config();
+require("dotenv").config();
 import { EvntComNode } from "evntcom-js/dist/node";
 import { ChatClient } from "@twurple/chat";
 import { PubSubClient, PubSubListener } from "@twurple/pubsub";
 import { ApiClient, HelixPrivilegedUser } from "@twurple/api";
 import { StaticAuthProvider } from "@twurple/auth";
+import { HelixBitsLeaderboardQuery } from "@twurple/api/lib/api/helix/bits/HelixBitsApi";
+import {
+  HelixCreateCustomRewardData,
+  HelixPaginatedCustomRewardRedemptionFilter,
+} from "@twurple/api/lib/api/helix/channelPoints/HelixChannelPointsApi";
+import {
+  HelixCustomRewardRedemptionStatus,
+  HelixCustomRewardRedemptionTargetStatus,
+} from "@twurple/api/lib/api/helix/channelPoints/HelixCustomRewardRedemption";
+import {
+  HelixClipFilter,
+  HelixPaginatedClipFilter,
+} from "@twurple/api/lib/api/helix/clip/HelixClipApi";
 import { ETwitchEvent } from "./ETwitchEvent";
 
-const NAME: string = process.env.EVNTBOARD_CONFIG_NAME || "twitch"
-const CLIENT_ID: string = process.env.EVNTBOARD_CONFIG_CLIENT_ID
-const TOKEN: string = process.env.EVNTBOARD_CONFIG_TOKEN
+const NAME: string = process.env.EVNTBOARD_NAME || "twitch";
+const HOST: string = process.env.EVNTBOARD_HOST || "localhost";
+const PORT: number = process.env.EVNTBOARD_PORT ? parseInt(process.env.EVNTBOARD_PORT) : 5001;
+const CLIENT_ID: string = process.env.EVNTBOARD_CONFIG_CLIENT_ID;
+const TOKEN: string = process.env.EVNTBOARD_CONFIG_TOKEN;
 
 const evntCom = new EvntComNode({
   name: NAME,
-  port: 5001,
-  host: 'localhost'
+  port: PORT,
+  host: HOST,
 });
+
+const registerAll = (obj: { [index: string]: any }) => {
+  for (let key in obj) {
+    evntCom.expose(key, obj[key]);
+  }
+};
 
 let chatClient: ChatClient;
 let pubSubClient: PubSubClient;
@@ -24,7 +45,7 @@ let bitsListener: PubSubListener<never>;
 let apiClient: ApiClient;
 
 evntCom.onOpen = async () => {
-  await unload() // ...
+  await unload(); // ...
   await evntCom.callMethod("newEvent", [
     ETwitchEvent.LOAD,
     null,
@@ -53,7 +74,7 @@ evntCom.onOpen = async () => {
   });
 
   chatClient.onDisconnect(async () => {
-    await unload()
+    await unload();
     await evntCom.callMethod("newEvent", [
       ETwitchEvent.CLOSE,
       null,
@@ -451,74 +472,234 @@ const unload = async () => {
 
 // chat API
 
-const say = async (message: string, reply: string) =>
-  chatClient?.say(currentUser.name, message, { replyTo: reply });
+registerAll({
+  // chat
+  say: async (message: string, reply: string) =>
+    chatClient?.say(currentUser.name, message, { replyTo: reply }),
+  me: async (message: string) => chatClient?.action(currentUser.name, message),
+  whisp: async (user: string, message: string) =>
+    chatClient?.whisper(user, message),
+  addVip: async (user: string) => chatClient?.addVip(currentUser.name, user),
+  ban: async (user: string, reason: string) =>
+    chatClient?.ban(currentUser.name, user, reason),
+  changeColor: async (color: string) => chatClient?.changeColor(color),
+  clear: async () => chatClient?.clear(currentUser.name),
+  disableEmoteOnly: async () => chatClient?.disableEmoteOnly(currentUser.name),
+  disableFollowersOnly: async () =>
+    chatClient?.disableFollowersOnly(currentUser.name),
+  disableR9k: async () => chatClient?.disableR9k(currentUser.name),
+  disableSlow: async () => chatClient?.disableSlow(currentUser.name),
+  disableSubsOnly: async () => chatClient?.disableSubsOnly(currentUser.name),
+  enableEmoteOnly: async () => chatClient?.enableEmoteOnly(currentUser.name),
+  enableFollowersOnly: async () =>
+    chatClient?.enableFollowersOnly(currentUser.name),
+  enableR9k: async () => chatClient?.enableR9k(currentUser.name),
+  enableSlow: async () => chatClient?.enableSlow(currentUser.name),
+  enableSubsOnly: async () => chatClient?.enableSubsOnly(currentUser.name),
+  getMods: async () => chatClient?.getMods(currentUser.name),
+  getVips: async () => chatClient?.getMods(currentUser.name),
+  host: async (channel: string) => chatClient?.host(currentUser.name, channel),
+  mod: async (user: string) => chatClient?.mod(currentUser.name, user),
+  purge: async (user: string, reason: string) =>
+    chatClient?.purge(currentUser.name, user, reason),
+  raid: async (channel: string) => chatClient?.raid(currentUser.name, channel),
+  removeVip: async (user: string) =>
+    chatClient?.removeVip(currentUser.name, user),
+  runCommercial: async (duration: 30 | 60 | 90 | 120 | 150 | 180) =>
+    chatClient?.runCommercial(currentUser.name, duration),
+  timeout: async (user: string, duration: number, reason: string) =>
+    chatClient?.timeout(currentUser.name, user, duration, reason),
+  unmod: async (user: string) => chatClient?.unmod(currentUser.name, user),
+  unraid: async (channel: string) => chatClient?.unraid(channel),
 
-const me = async (message: string) => chatClient?.action(currentUser.name, message);
+  // Bits
 
-const whisp = async (user: string, message: string) =>
-  chatClient?.whisper(user, message);
+  bitsGetLeaderboard: async (data?: HelixBitsLeaderboardQuery) => {
+    return await apiClient.bits.getLeaderboard(data);
+  },
+  bitsGetCheermotes: async (channel: string) => {
+    return await apiClient.bits.getCheermotes(channel);
+  },
 
-evntCom.expose("say", say);
-evntCom.expose("me", me);
-evntCom.expose("whisp", whisp);
+  // Channel
+  channelGetChannelEditors: async () => {
+    return await apiClient.channels.getChannelEditors(currentUser.id);
+  },
+  channelGetInfo: async () => {
+    return await apiClient.channels.getChannelInfo(currentUser.id);
+  },
+  channelUpdateTitle: async (title: string) => {
+    return await apiClient.channels.updateChannelInfo(currentUser.id, {
+      title,
+    });
+  },
+  channelUpdateGame: async (game: string) => {
+    let gameObj = await apiClient.games.getGameByName(game);
+    return await apiClient.channels.updateChannelInfo(currentUser.id, {
+      gameId: gameObj?.id || game,
+    });
+  },
+  channelUpdateLanguage: async (language: string) => {
+    return await apiClient.channels.updateChannelInfo(currentUser.id, {
+      language,
+    });
+  },
+  channelStartCommercial: async (duration: 30 | 60 | 90 | 120 | 150 | 180) => {
+    return await apiClient.channels.startChannelCommercial(
+      currentUser.id,
+      duration
+    );
+  },
 
-// Helix Channel
+  // ChannelPointsApi
+  channelPointsGetCustomRewards: async (onlyManageable?: boolean) => {
+    return await apiClient.channelPoints.getCustomRewards(
+      currentUser.id,
+      onlyManageable
+    );
+  },
+  channelPointsGetCustomRewardsByIds: async (rewardIds: string[]) => {
+    return await apiClient.channelPoints.getCustomRewardsByIds(
+      currentUser.id,
+      rewardIds
+    );
+  },
+  channelPointsGetCustomRewardById: async (rewardId: string) => {
+    return await apiClient.channelPoints.getCustomRewardById(
+      currentUser.id,
+      rewardId
+    );
+  },
+  channelPointsCreateCustomReward: async (
+    rewardData: HelixCreateCustomRewardData
+  ) => {
+    return await apiClient.channelPoints.createCustomReward(
+      currentUser.id,
+      rewardData
+    );
+  },
+  channelPointsUpdateCustomReward: async (
+    rewardId: string,
+    rewardData: HelixCreateCustomRewardData
+  ) => {
+    return await apiClient.channelPoints.updateCustomReward(
+      currentUser.id,
+      rewardId,
+      rewardData
+    );
+  },
+  channelPointsDeleteCustomReward: async (rewardId: string) => {
+    return await apiClient.channelPoints.deleteCustomReward(
+      currentUser.id,
+      rewardId
+    );
+  },
+  channelPointsGetRedemptionsByIds: async (
+    rewardId: string,
+    redemptionIds: string[]
+  ) => {
+    return await apiClient.channelPoints.getRedemptionsByIds(
+      currentUser.id,
+      rewardId,
+      redemptionIds
+    );
+  },
+  channelPointsGetRedemptionById: async (
+    rewardId: string,
+    redemptionId: string
+  ) => {
+    return await apiClient.channelPoints.getRedemptionById(
+      currentUser.id,
+      rewardId,
+      redemptionId
+    );
+  },
+  channelPointsGetRedemptionsForBroadcaster: async (
+    rewardId: string,
+    status: HelixCustomRewardRedemptionStatus,
+    filter: HelixPaginatedCustomRewardRedemptionFilter
+  ) => {
+    return await apiClient.channelPoints.getRedemptionsForBroadcaster(
+      currentUser.id,
+      rewardId,
+      status,
+      filter
+    );
+  },
+  channelPointsGetRedemptionsForBroadcasterPaginated: async (
+    rewardId: string,
+    status: HelixCustomRewardRedemptionStatus,
+    filter: HelixPaginatedCustomRewardRedemptionFilter
+  ) => {
+    return apiClient.channelPoints.getRedemptionsForBroadcasterPaginated(
+      currentUser.id,
+      rewardId,
+      status,
+      filter
+    );
+  },
+  channelPointsUpdateRedemptionStatusByIds: async (
+    rewardId: string,
+    redemptionIds: string[],
+    status: HelixCustomRewardRedemptionTargetStatus
+  ) => {
+    return apiClient.channelPoints.updateRedemptionStatusByIds(
+      currentUser.id,
+      rewardId,
+      redemptionIds,
+      status
+    );
+  },
 
-const channelGetChannelEditors = async () => {
-  return await apiClient.channels.getChannelEditors(currentUser.id);
-};
+  // CLip
+  clipGetClipsForBroadcaster: async (filter?: HelixPaginatedClipFilter) => {
+    return apiClient.clips.getClipsForBroadcaster(currentUser.id, filter);
+  },
+  getClipsForBroadcasterPaginated: async (filter?: HelixClipFilter) => {
+    return apiClient.clips.getClipsForBroadcasterPaginated(
+      currentUser.id,
+      filter
+    );
+  },
+  getClipsForGame: async (
+    gameId: string,
+    filter?: HelixPaginatedClipFilter
+  ) => {
+    return apiClient.clips.getClipsForGame(gameId, filter);
+  },
+  getClipsForGamePaginated: async (
+    gameId: string,
+    filter?: HelixClipFilter
+  ) => {
+    return apiClient.clips.getClipsForGamePaginated(gameId, filter);
+  },
+  getClipsByIds: async (ids: string[]) => {
+    return apiClient.clips.getClipsByIds(ids);
+  },
+  getClipById: async (id: string) => {
+    return apiClient.clips.getClipById(id);
+  },
+  createClip: async (createAfterDelay?: boolean) => {
+    return apiClient.clips.createClip({
+      channelId: currentUser.id,
+      createAfterDelay,
+    });
+  },
 
-const channelGetInfo = async () => {
-  return await apiClient.channels.getChannelInfo(currentUser.id)
-};
-
-const channelUpdateTitle = async (title: string) => {
-  return await apiClient.channels.updateChannelInfo(currentUser.id, { title });
-};
-
-const channelUpdateGame = async (game: string) => {
-  let gameObj = await apiClient.games.getGameByName(game);
-  return await apiClient.channels.updateChannelInfo(currentUser.id, {
-    gameId: gameObj?.id || game,
-  });
-};
-
-const channelUpdateLanguage = async (language: string) => {
-  return await apiClient.channels.updateChannelInfo(currentUser.id, {
-    language,
-  });
-};
-
-const channelStartCommercial = async (
-  duration: 30 | 60 | 90 | 120 | 150 | 180
-) => {
-  return await apiClient.channels.startChannelCommercial(
-    currentUser.id,
-    duration
-  );
-};
-
-const usersGetUserByName = async (user: string) => {
-  const data = await apiClient.users.getUserByName(user);
-  return {
-    broadcasterType: data.broadcasterType,
-    creationDate: data.creationDate,
-    description: data.description,
-    displayName: data.displayName,
-    id: data.id,
-    name: data.name,
-    offlinePlaceholderUrl: data.offlinePlaceholderUrl,
-    profilePictureUrl: data.profilePictureUrl,
-    type: data.type,
-    views: data.views,
-  };
-};
-
-evntCom.expose("channelGetInfo", channelGetInfo);
-evntCom.expose("channelGetChannelEditors", channelGetChannelEditors);
-evntCom.expose("channelUpdateTitle", channelUpdateTitle);
-evntCom.expose("channelUpdateGame", channelUpdateGame);
-evntCom.expose("channelUpdateLanguage", channelUpdateLanguage);
-evntCom.expose("channelStartCommercial", channelStartCommercial);
-evntCom.expose("usersGetUserByName", usersGetUserByName);
+  // Users
+  usersGetUserByName: async (user: string) => {
+    const data = await apiClient.users.getUserByName(user);
+    return {
+      broadcasterType: data.broadcasterType,
+      creationDate: data.creationDate,
+      description: data.description,
+      displayName: data.displayName,
+      id: data.id,
+      name: data.name,
+      offlinePlaceholderUrl: data.offlinePlaceholderUrl,
+      profilePictureUrl: data.profilePictureUrl,
+      type: data.type,
+      views: data.views,
+    };
+  },
+});
